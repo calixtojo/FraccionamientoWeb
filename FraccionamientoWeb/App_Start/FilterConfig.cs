a029b15b6045740c866643d755f111156f0624ab
Jose Alfonso Eduardo Calixto Ramirez <jose.calixto@provident.com.mx>
2021-03-08 23:27:56 -0600

    Agregar archivos de proyecto.

---
+++ FraccionamientoWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FraccionamientoWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
